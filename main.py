f3ba5ed7f1f2ed4e2d8900545f302ab653dfff8c
sebekdabek <sebekdabek@gmail.com>
2020-05-27 17:19:16 +0200

    Commiting latest changes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import sys
 import lexer
-import parser
+from parser import parser
 
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(
-            sys.argv) > 1 else "inputFiles/helloworld.c"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "inputFiles/helloworld.c"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
